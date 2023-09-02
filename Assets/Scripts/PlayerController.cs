08651dba6c67230e5770f6430513a7d135a5fac0
bheifetz <bheifetz94@gmail.com>
2023-09-02 13:30:32 -0700

    BHeifetzMULT152Unit1LessonD
    Ben Heifetz, Lesson D

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 15.0f;
-    
+
+    // The funny numbers that say how fast the car goes vroom and turns
+    private float speed = 15.0f;
+    private float turnSpeed = 60.0f;
+
+    private float horizontalInput;
+    private float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +21,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         Debug.Log(Time.deltaTime);
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // These are what make the car go vroom and turn
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
